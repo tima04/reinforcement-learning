e33c0fd2dbee25ebd9e0ee3f9ac166fa81ee22a7
amit <amit.kothiyal2004@gmail.com>
2016-05-11 14:16:35 +0200

    fixed bugs in TetrisFeatures

--- src/main/java/domains/tetris/TetrisFeatures.java
+++ src/main/java/domains/tetris/TetrisFeatures.java
@@ -32,6 +32,10 @@ public class TetrisFeatures {
 	public final int nPatternDiversity;
 	public final int nClearedLines;
 	public final int nBrickCleared;
+
+	public static void main(String[] args) {
+		System.out.println("text: foo");
+	}
 	
 	TetrisFeatures(Builder builder) {
 		this.height = builder.height;
@@ -122,16 +126,16 @@ public class TetrisFeatures {
 
 		//at top of each column nwells are
 		//max(min(colheightLeft, colheightRight), colheightright) - colHeight
-		ToIntBiFunction<Integer, Integer> max = (x, y) -> x > y ? x : y;	
-		ToIntBiFunction<Integer, Integer> min = (x, y) -> x < y ? x : y;	
+		// ToIntBiFunction<Integer, Integer> max = (x, y) -> x > y ? x : y;	
+		// ToIntBiFunction<Integer, Integer> min = (x, y) -> x < y ? x : y;	
 		for (int i = 0; i < this.width; i++) {
 			int maxmin;
 			if (i == 0)
-				maxmin = max.applyAsInt(colHeights[1], colHeights[i]);
+				maxmin = Math.max(colHeights[1], colHeights[i]);
 			else if (i == this.width-1)
-				maxmin = max.applyAsInt(colHeights[i-1], colHeights[i]);
+				maxmin = Math.max(colHeights[i-1], colHeights[i]);
 			else
-				maxmin = max.applyAsInt(min.applyAsInt(colHeights[i-1], colHeights[i+1]),
+				maxmin = Math.max(Math.min(colHeights[i-1], colHeights[i+1]),
 										colHeights[i]);
 			rslt += maxmin - colHeights[i];
 		} 
@@ -176,8 +180,10 @@ public class TetrisFeatures {
 	}
 
 	int getRowTransition() {
-		//each consecutive sequence of holes (from left to right) contributes 2,
+		//each consecutive sequence of holes (from left to right) contributes 2(except *) 
 		//"f h h f", will contribute same as "f h f", (f is filled and h is hole).
+		//* if a hole height is larger then min(colheightleft, colheightright) then
+		// that hole contribution is taken into account by change in colheights.
 
 		// moving from left to right change in colHeight by amount delta contributes,
 		// delta, its also assumed that left and right wall are full cols, so for
@@ -185,10 +191,16 @@ public class TetrisFeatures {
 		// contribution will be |4-2| + |2-3| + |3-2| + |2-3| + |2-4| = 6.
 		int rslt = 0;
 
-		//for each hole if right cell is not a hole increment rslt by 2.
+		//for each hole if right cell is not a hole and hole height is less than
+		// min height of left and right column increment rslt by 2.
 		for (Pair<Integer, Integer> hole : holesCords ) {
-			if (! holesCords.contains(new Pair(hole.getFirst(), hole.getSecond()+1)))
-								rslt += 2;
+			int x = hole.getFirst();
+			int y = hole.getSecond();
+			// minimum between left and right column height. 
+			int minHight = x == 0 ? colHeights[x+1] : x == width-1 ? colHeights[x-1] :
+				Math.min(colHeights[x-1], colHeights[x+1]);
+			if (y > minHight && (! holesCords.contains(new Pair(x, y+1))))
+				rslt += 2;
 		}
 
 		//each delta diff in consecutive colHeights will contribute delta.
@@ -211,10 +223,10 @@ public class TetrisFeatures {
 			if (! holesCords.contains(new Pair(hole.getFirst()+1, hole.getSecond())))
 								rslt += 2;
 		}
-		//for each column if it's height is less then board height increment rslt by 1.
-		for (int ch : colHeights)
-			if (ch < this.height)
-				rslt++;
+		// for each column if it's height is less then board height increment rslt by 1.
+		// since there is no ceiling so this holds for all columns. 
+		rslt += width;
+
 		return rslt;
 	}
 
