45b82ac1583b660a988d98ba8b593d359b81a5a3
simonalgorta <simalgorta@gmail.com>
2016-05-12 08:51:50 +0200

    cumwells test passed

--- src/main/java/domains/tetris/TetrisFeatures.java
+++ src/main/java/domains/tetris/TetrisFeatures.java
@@ -120,7 +120,7 @@ public class TetrisFeatures {
 			int heightLeft = hc.getSecond() == 0 ? height : colHeights[hc.getSecond()-1];
 			int heightRight = hc.getSecond() == width - 1 ? height : colHeights[hc.getSecond()+1];
 			if (!(holesCords.contains(left) || holesCords.contains(right)) &&
-					(heightLeft >= hc.getFirst() && heightRight >= hc.getFirst()))
+					(heightLeft > hc.getFirst() && heightRight > hc.getFirst()))
 				rslt = rslt + getWellDepth(hc);
 			}
 
@@ -129,24 +129,18 @@ public class TetrisFeatures {
 		// ToIntBiFunction<Integer, Integer> max = (x, y) -> x > y ? x : y;	
 		// ToIntBiFunction<Integer, Integer> min = (x, y) -> x < y ? x : y;	
 		for (int i = 0; i < this.width; i++) {
-			int maxmin;
-			if (i == 0)
-				maxmin = Math.max(colHeights[1], colHeights[i]);
-			else if (i == this.width-1)
-				maxmin = Math.max(colHeights[i-1], colHeights[i]);
-			else
-				maxmin = Math.max(Math.min(colHeights[i-1], colHeights[i+1]),
-										colHeights[i]);
+			int heightLeft = i == 0 ? height : colHeights[i-1];
+			int heightRight = i == width - 1 ? height : colHeights[i+1];
+			int maxmin = Math.max(Math.min(heightLeft, heightRight), colHeights[i]);
 			int cum = 0;
-			boolean well = false;
-			for (int r = maxmin-1; r >= colHeights[i] ; r--) { //We iterate from the top of the well to the bottom
+			for (int r = colHeights[i]; r <= maxmin-1 ; r++) { //We iterate from the top of the well to the bottom
 				boolean holeLeft = i == 0 ? false : holesCords.contains(new Pair<>(r, i-1));
 				boolean holeRight = i == width -1 ? false : holesCords.contains(new Pair<>(r, i+1));
-				if(!holeLeft && !holeRight) {//This is a well
+				if((!holeLeft && !holeRight) &&
+						heightLeft >= r && heightRight >= r) {//This is a well
 					cum++;//we accumulate
 					rslt += cum;//we sum
-					well = true;
-				}else if(well){//this is not a well but there was already a well above and it is an empty cell
+				}else{//this is not a well but there was already a well above and it is an empty cell
 					cum++;//we only accumulate
 				}
 			}
@@ -173,8 +167,8 @@ public class TetrisFeatures {
 		int rslt = 1;
 			//for each hole directly below add 1 to the rslt.
 			for (int i = hc.getFirst();
-				 holesCords.contains(new Pair(i+1, hc.getSecond()));
-					 i++)
+				 holesCords.contains(new Pair(i-1, hc.getSecond()));
+					 i--)
 				rslt++;
 
 		return rslt;
